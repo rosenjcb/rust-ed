05cb30886d0a7d2bb767f8245314640f08cbf939
Necroforger <Necroforger@gmail.com>
2019-10-26 22:42:06 -0400

    Create a clipboard trait
    It might be useful for the copy and paste methods to return a `Result` if failure is a possibility.

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,11 +1,38 @@
-//Just  a wrapper around a String for now... maybe it'll hold formatting someday.
-pub struct Clipboard {
+// TODO: create an implementation of clipboard that interfaces with the operating system
+
+/// The clipboard trait is responsible for storing and retrieving a buffer of characters
+pub trait Clipboard {
+    /// Return the contents of the clipboard as a string
+    fn paste(&self) -> String;
+
+    /// Replace the current stored data in the clipboard with `content`
+    fn copy<T>(&mut self, content: T)
+    where
+        T: AsRef<str>;
+}
+
+/// Just  a wrapper around a String for now... maybe it'll hold formatting someday.
+pub struct MemoryClipboard {
     pub inner: String
 }
 
-impl Clipboard {
+/// MemoryClipboard is an in memory implementation of the clipboard trait
+impl MemoryClipboard {
     pub fn new() -> Self {
         let inner = String::from("");
-        Clipboard { inner }
+        Self { inner }
+    }
+}
+
+impl Clipboard for MemoryClipboard {
+    fn paste(&self) -> String {
+        return self.inner.clone();
+    }
+
+    fn copy<T>(&mut self, content: T)
+    where
+        T: AsRef<str>,
+    {
+        self.inner = String::from(content.as_ref());
     }
 }
